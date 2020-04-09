c9fa5d72b7f4fab02b955ba245eea55d62833e75
mtics <computer.science@foxmail.com>
2020-04-09 10:51:58 +0800

    add rules in gitignore

--- auto_sign.py
+++ auto_sign.py
@@ -12,9 +12,11 @@ def sign_in(uid, pwd):
     chrome_options.add_argument("--headless")
     # simulate a browser to open the website
     browser = webdriver.Chrome(options=chrome_options)
+    # browser = webdriver.Chrome()
     browser.get("https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/first0")
 
     # input uid and password
+    print("正在输入用户{0}的用户名和密码".format(uid))
     browser.find_element_by_xpath("//*[@id='mt_5']/div[1]/div[3]/input").send_keys(uid)
     browser.find_element_by_xpath("//*[@id='mt_5']/div[2]/div[3]/input").send_keys(pwd)
 
@@ -26,21 +28,29 @@ def sign_in(uid, pwd):
     real_mid_page_url = browser.find_element_by_xpath("//*[@id='zzj_top_6s']").get_attribute("src")
     browser.get(real_mid_page_url)
 
+    print("正在检查用户{0}的签到状态".format(uid))
     msg = browser.find_element_by_xpath("//*[@id='bak_0']/div[7]/span").text
     if msg == "今日您已经填报过了":
         return msg
 
     # click to fill in
-    browser.find_element_by_xpath("//*[@id='bak_0']/div[13]/div[3]/div[6]").click()
+    span_text = browser.find_element_by_xpath("//*[@id='bak_0']/div[13]/div[3]/div[4]/span").text
+    if span_text == "本人填报":
+        browser.find_element_by_xpath("//*[@id='bak_0']/div[13]/div[3]/div[4]").click()
+    else:
+        browser.find_element_by_xpath("//*[@id='bak_0']/div[13]/div[3]/div[6]").click()
+
     time.sleep(2)
 
     # click to submit
+    print("正在为用户{0}进行签到".format(uid))
     browser.find_element_by_xpath("//*[@id='bak_0']/div[19]/div[4]").click()
     time.sleep(2)
 
     final_text = browser.find_element_by_xpath("//*[@id='bak_0']/div[2]/div[2]/div[2]/div[2]").text
 
     # quit the browser
+    print("用户{0}的签到已结束".format(uid))
     browser.quit()
     return final_text
 
@@ -48,10 +58,12 @@ def sign_in(uid, pwd):
 if __name__ == "__main__":
 
     # For Single User
-    # msg = sign_in(UID, PWD)
-    # mail.mail(msg, EMAIL_TO)
+    msg = sign_in(UID, PWD)
+    mail.mail(msg, EMAIL_TO)
 
     # For Multiple Users
     for user in users:
         msg = sign_in(user.uid, user.pwd)
+        print("即将发送邮件通知用户{0}".format(user.uid))
         mail.mail(msg, user.email)
+        print("邮件发送已结束")
